2ee6bc49598a0bce9ef8e375a39e1216b93a5e67
unknown <zhuxiaoxiang@cn.net.ntes>
2019-04-23 21:17:02 +0800

    Initial commit

---
+++ src/main/java/hello2/ParseHtml2.java
@@ -0,0 +1,12 @@
+package hello2;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+/**
+ * 用以解析某网页
+ */
+public class ParseHtml2 {
+    public static void main(String[]args){
+    }
+    public static void outpout(){}
+}
