e645094578e5532632734f42745b20c7b3222c80
zxx <1806320505@qq.com>
2019-05-08 07:55:18 +0800

    add test dir

---
+++ src/main/java/test/ParseHtml2.java
@@ -0,0 +1,13 @@
+﻿package test;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+/**
+ * 用以解析某网页
+ */
+public class ParseHtml2 {
+    public static void main(String[]args){
+	System.out.println("add ...");
+    }
+    public static void outpout(){}
+}
