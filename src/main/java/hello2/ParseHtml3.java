1068c4579ede35f89a3d58bae9174073dc87675b
zxx <1806320505@qq.com>
2019-05-24 12:11:22 +0800

    add a test in ParseHtml3

--- src/main/java/hello2/ParseHtml3.java
+++ src/main/java/hello2/ParseHtml3.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 /**
  * 用以解析某网页
  */
-public class ParseHtml1 {
+public class ParseHtml3 {
     public static void main(String[]args){
     }
     public static void outpout(){
-        System.out.println("this is a good news for test1");
+        System.out.println("this is a good news for test3");
     }
 }
