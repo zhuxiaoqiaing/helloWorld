af296218d10fec29a8a1631d44b47ae7a42d84fc
zxx <1806320505@qq.com>
2019-05-24 12:08:03 +0800

    add a test in ParseHtml1

--- src/main/java/hello2/ParseHtml1.java
+++ src/main/java/hello2/ParseHtml1.java
@@ -9,6 +9,6 @@ public class ParseHtml1 {
     public static void main(String[]args){
     }
     public static void outpout(){
-        System.out.println("this is a good news");
+        System.out.println("this is a good news for test");
     }
 }
