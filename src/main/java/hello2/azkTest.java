2134f508156104a67367af8388f68979c3bed749
zxx <1806320505@qq.com>
2019-06-27 20:36:30 +0800

    add two

---
+++ src/main/java/hello2/azkTest.java
@@ -0,0 +1,11 @@
+package hello2;
+
+/**
+ * Author: zhuxiaoxiang
+ * Date: 2019/6/27 20:35
+ */
+public class azkTest {
+    public static void main(String[] args) {
+        System.out.println("add two");
+    }
+}
